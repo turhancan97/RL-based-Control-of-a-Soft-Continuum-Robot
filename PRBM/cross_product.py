79db9c76ffdcc4982ce866d4990c90bcb10a6776
turhancan97 <turhancan.kargin@gmail.com>
2022-04-13 13:59:26 +0200

    all files deleted

--- PRBM/cross_product.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def cross_product(u,v):
-    #returns cross product of columns of u with columns of v
-    res=np.array([np.multiply(u[1,:],v[2,:]) - np.multiply(v[1,:],u[2,:]),
-                  np.multiply(u[2,:],v[0,:]) - np.multiply(v[2,:],u[0,:]),
-                  np.multiply(u[0,:],v[1,:]) - np.multiply(v[0,:],u[1,:])]);
-    return res
