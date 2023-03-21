0402eacda7bf13568ead3c989bd6a4cb1a521181
turhancan97 <turhancan.kargin@gmail.com>
2023-03-21 13:08:04 +0100

    layers configuration

--- Pytorch/model.py
+++ Pytorch/model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=512, fc2_units=512,fc3_units=256,fc4_units=256):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=32,fc3_units=16,fc4_units=8):
         """Initialize parameters and build model.
         Params
         ======
@@ -50,7 +50,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=512, fc2_units=256, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, fcs1_units=64, fc2_units=32, fc3_units=16):
         """Initialize parameters and build model.
         Params
         ======
