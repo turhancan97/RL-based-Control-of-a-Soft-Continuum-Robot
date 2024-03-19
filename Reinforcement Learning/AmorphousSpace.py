ba577801b168fe21872749cb9b7f171752c87e17
Turhan Can Kargin <turhancan.kargin@gmail.com>
2024-03-19 15:14:03 +0300

    Add circles.txt with circle coordinates and radii

--- Reinforcement Learning/AmorphousSpace.py
+++ Reinforcement Learning/AmorphousSpace.py
@@ -1,4 +1,11 @@
-# %%
+import os
+
+# Get the absolute path to the directory containing this script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Construct the absolute path to the file
+file_path = os.path.join(dir_path, 'circles.txt')
+
 import numpy as np
 from gym import spaces
 
@@ -19,88 +26,12 @@ class AmorphousSpace(spaces.Space):
           - 'radius' : float
                 The radius of the circle.
         """
-
-        circles = [{'center': np.array([-0.1, -0.07]), 'radius': 0.03},
-           {'center': np.array([-0.08, -0.07]), 'radius': 0.023},
-           {'center': np.array([-0.15, 0.00]), 'radius': 0.025},
-           {'center': np.array([-0.15, -0.07]), 'radius': 0.03},
-           {'center': np.array([-0.155, 0.01]), 'radius': 0.025},
-           {'center': np.array([-0.16, 0.02]), 'radius': 0.025},
-           {'center': np.array([-0.165, 0.03]), 'radius': 0.025},
-           {'center': np.array([-0.13, -0.04]), 'radius': 0.025},
-           {'center': np.array([-0.15, 0.06]), 'radius': 0.025},
-           {'center': np.array([-0.15, 0.17]), 'radius': 0.06},
-           {'center': np.array([-0.12, 0.20]), 'radius': 0.025},
-           {'center': np.array([-0.196, 0.1]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.0]), 'radius': 0.04},
-           {'center': np.array([-0.2, 0.09]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.08]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.07]), 'radius': 0.065},
-           {'center': np.array([-0.2, 0.07]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.06]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.05]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.04]), 'radius': 0.06},
-           {'center': np.array([-0.2, 0.03]), 'radius': 0.06},
-           {'center': np.array([-0.205, 0.05]), 'radius': 0.06},
-           {'center': np.array([-0.16, -0.05]), 'radius': 0.04},
-           {'center': np.array([-0.17, 0.17]), 'radius': 0.05},
-           {'center': np.array([-0.15, 0.12]), 'radius': 0.03},
-           {'center': np.array([-0.143, 0.16]), 'radius': 0.04},
-           {'center': np.array([-0.15, 0.20]), 'radius': 0.044},
-           {'center': np.array([-0.2, 0.15]), 'radius': 0.035},
-           {'center': np.array([-0.21, 0.14]), 'radius': 0.033},
-           {'center': np.array([-0.12, -0.07]), 'radius': 0.033},
-           {'center': np.array([-0.18, -0.03]), 'radius': 0.043},
-           {'center': np.array([-0.10, 0.25]), 'radius': 0.03},
-           {'center': np.array([-0.10, 0.23]), 'radius': 0.03},
-           {'center': np.array([-0.13, 0.235]), 'radius': 0.027},
-           {'center': np.array([-0.06, 0.26]), 'radius': 0.025},
-           {'center': np.array([-0.06, 0.268]), 'radius': 0.025},
-           {'center': np.array([-0.07, 0.268]), 'radius': 0.023},
-           {'center': np.array([-0.05, 0.268]), 'radius': 0.023},
-           {'center': np.array([-0.04, 0.268]), 'radius': 0.023},
-           {'center': np.array([-0.04, 0.272]), 'radius': 0.023},
-           {'center': np.array([-0.03, 0.273]), 'radius': 0.021},
-           {'center': np.array([-0.02, 0.275]), 'radius': 0.019},
-           {'center': np.array([-0.02, 0.278]), 'radius': 0.020},
-           {'center': np.array([-0.015, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.015, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.012, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.011, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.010, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.009, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.008, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.007, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.006, 0.28]), 'radius': 0.020},
-           {'center': np.array([-0.005, 0.28]), 'radius': 0.020},
-           {'center': np.array([0.00, 0.28]), 'radius': 0.0150},
-           {'center': np.array([0.005, 0.285]), 'radius': 0.0150},
-           {'center': np.array([0.0075, 0.285]), 'radius': 0.0150},
-           {'center': np.array([0.01, 0.285]), 'radius': 0.0150},
-           {'center': np.array([0.015, 0.285]), 'radius': 0.0150},
-           {'center': np.array([0.02, 0.285]), 'radius': 0.0150},
-           {'center': np.array([0.03, 0.285]), 'radius': 0.012},
-           {'center': np.array([0.04, 0.285]), 'radius': 0.012},
-           {'center': np.array([0.05, 0.285]), 'radius': 0.010},
-           {'center': np.array([0.06, 0.28]), 'radius': 0.012},
-           {'center': np.array([0.07, 0.28]), 'radius': 0.012},
-           {'center': np.array([0.08, 0.275]), 'radius': 0.012},
-           {'center': np.array([0.09, 0.276]), 'radius': 0.01},
-           {'center': np.array([0.1, 0.275]), 'radius': 0.01},
-           {'center': np.array([0.11, 0.27]), 'radius': 0.01},
-           {'center': np.array([0.12, 0.265]), 'radius': 0.01},
-           {'center': np.array([0.13, 0.255]), 'radius': 0.01},
-           {'center': np.array([0.14, 0.245]), 'radius': 0.01},
-           {'center': np.array([0.15, 0.235]), 'radius': 0.01},
-           {'center': np.array([-0.2, 0.000]), 'radius': 0.05},
-           {'center': np.array([-0.2, -0.0150]), 'radius': 0.045},
-           {'center': np.array([-0.15, 0.075]), 'radius': 0.025},
-           {'center': np.array([-0.10, 0.2]), 'radius': 0.020},
-           {'center': np.array([-0.080, 0.21]), 'radius': 0.020},
-           {'center': np.array([-0.22, 0.17]), 'radius': 0.01},
-           {'center': np.array([-0.215, 0.177]), 'radius': 0.012},
-           {'center': np.array([-0.225, 0.165]), 'radius': 0.01},
-           {'center': np.array([-0.125, 0.225]), 'radius': 0.045},]
+        # read circles from a circles.txt file
+        circles = []
+        with open(file_path, 'r') as f:
+            for line in f:
+                x, y, r = line.strip().split(',')
+                circles.append({'center': np.array([float(x), float(y)]), 'radius': float(r)})
 
         self.circles = circles
         self.low = np.array([circle['center'][0] - circle['radius'] for circle in circles])
