1c574d5adcb2ba22c6a3aa525756425ac6daac1f
Turhan Can Kargin <turhancan.kargin@gmail.com>
2024-03-28 19:15:18 +0100

    Add new Keras models and reward files

--- Keras/reward_visualization/reward_vis.py
+++ Keras/reward_visualization/reward_vis.py
@@ -1,25 +1,34 @@
 # %%
 import sys
-sys.path.append('../../')
+
+sys.path.append("../../")
 
 from continuum_robot.utils import *
 
 import yaml
 
 # load config file
-with open('config.yaml', 'r') as file:
+with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 # %%
-avg_reward_list = load_pickle_file(f'../{config["goal_type"]}/{config["reward_type"]}/rewards/avg_reward_list')
-ep_reward_list = load_pickle_file(f'../{config["goal_type"]}/{config["reward_type"]}/rewards/ep_reward_list')
+avg_reward_list = load_pickle_file(
+    f'../{config["goal_type"]}/{config["reward_type"]}/rewards/avg_reward_list'
+)
+ep_reward_list = load_pickle_file(
+    f'../{config["goal_type"]}/{config["reward_type"]}/rewards/ep_reward_list'
+)
 
 # %%
 ## Plotting graph
 reward_visualization(ep_reward_list, avg_reward_list)
-# plt.savefig('../../../docs/images/random_goal/reward_keras_er_comp.png')
+plt.savefig(
+    f'../../docs/images/{config["reward_type"]}/reward_keras_{config["reward_type"]}.png'
+)
 plt.show()
 # %%
 # Plotting graph log scale
 reward_log10_visualization(ep_reward_list, avg_reward_list)
-# plt.savefig('../../../docs/images/random_goal/reward_log_keras_er_comp.png')
+plt.savefig(
+    f'../../docs/images/{config["reward_type"]}/reward_log_keras_{config["reward_type"]}.png'
+)
 plt.show()
