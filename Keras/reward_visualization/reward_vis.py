b07a4c83c32cfc38e567bb8385ed2a2bc4b445f1
Turhan Can Kargin <turhancan.kargin@gmail.com>
2024-03-28 20:26:59 +0100

    Update reward visualization code

--- Keras/reward_visualization/reward_vis.py
+++ Keras/reward_visualization/reward_vis.py
@@ -21,14 +21,14 @@ ep_reward_list = load_pickle_file(
 # %%
 ## Plotting graph
 reward_visualization(ep_reward_list, avg_reward_list)
-plt.savefig(
-    f'../../docs/images/{config["goal_type"]}/reward_keras_{config["reward_type"]}.png'
-)
+# plt.savefig(
+#     f'../../docs/images/{config["goal_type"]}/reward_keras_{config["reward_type"]}.png'
+# )
 plt.show()
 # %%
 # Plotting graph log scale
 reward_log10_visualization(ep_reward_list, avg_reward_list)
-plt.savefig(
-    f'../../docs/images/{config["goal_type"]}/reward_log_keras_{config["reward_type"]}.png'
-)
+# plt.savefig(
+#     f'../../docs/images/{config["goal_type"]}/reward_log_keras_{config["reward_type"]}.png'
+# )
 plt.show()
