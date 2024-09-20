b3adff77c1fdc6cb899a27a0a41d903abd92cdb5
Turhan Can Kargin <turhancan.kargin@gmail.com>
2024-09-20 20:37:16 +0200

    Refactor code to read config file and use numpy arrays for error calculations

--- Pytorch/ddpg.py
+++ Pytorch/ddpg.py
@@ -13,13 +13,28 @@ from ddpg_agent import Agent
 from env import continuumEnv
 import time
 import math
+import os
+import yaml
+
+# * Read the config file
+# Get the absolute path to the directory containing this script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# Construct the absolute path to the file
+file_path = os.path.join(dir_path, "config.yaml")
+# load config file
+with open(file_path, "r") as file:
+    config = yaml.safe_load(file)
+
+start_time = time.time()
+
+TRAIN = False
 
 env = continuumEnv()
-env.seed(10)
+# env.seed(10)
 agent = Agent(state_size=4, action_size=3, random_seed=10)
 
 # %%
-def ddpg(n_episodes=1500, max_t=750, print_every=25):
+def ddpg(n_episodes=300, max_t=750, print_every=25):
     global scores
     global avg_reward_list
     scores_deque = deque(maxlen=print_every)
@@ -40,14 +55,15 @@ def ddpg(n_episodes=1500, max_t=750, print_every=25):
             print("===============================================================")
             print("Goal Kappas are ",[env.target_k1,env.target_k2,env.target_k3])
             print("===============================================================")
-            time.sleep(2)
+            time.sleep(0.5)
         
         for t in range(max_t):
             action = agent.act(state)
-            # next_state, reward, done, _ = env.step_minus_euclidean_square(action) # -e^2
-            next_state, reward, done, _ = env.step_error_comparison(action) # reward is -1.00 or -0.50 or 1.00
-            # next_state, reward, done, _ = env.step_minus_weighted_euclidean(action) # -0.7*e
-            # next_state, reward, done, _ = env.step_distance_based(action) # reward is du-1 - du
+            # 'step_minus_euclidean_square' is e^2
+            # 'step_minus_weighted_euclidean' is 0.7*e
+            # 'step_error_comparison' is -1.00 or -0.50 or 1.00
+            # 'step_distance_based' is du-1 - du
+            next_state, reward, done, _ = env.step(action, reward_function = config['reward']['function'])
             agent.step(state, action, reward, next_state, done) 
             state = next_state
             # # Uncomment below!!!!
@@ -71,26 +87,32 @@ def ddpg(n_episodes=1500, max_t=750, print_every=25):
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
         torch.save(agent.actor_local.state_dict(), 'experiment/checkpoint_actor.pth')
         torch.save(agent.critic_local.state_dict(), 'experiment/checkpoint_critic.pth')
-            
+    print('\n')
+    print(f'{counter} times robot reached the target point in total {n_episodes} episodes')
+    end_time = time.time() - start_time
+    print('Total Overshoot 0: ', env.overshoot0)
+    print('Total Overshoot 1: ', env.overshoot1)
+    print(f'Total Elapsed Time is {int(end_time)/60} minutes')
+                
     return scores
 
-scores = ddpg()
+if TRAIN:
+    scores = ddpg()
 
-# %% Result
-plt.subplot(1, 2, 1)
-plt.plot(np.arange(1, len(scores)+1), scores)
-plt.ylabel('Reward')
-plt.xlabel('Episode #')
-with open('experiment/scores.pickle', 'wb') as f:
-    # Pickle the 'data' dictionary using the highest protocol available.
-    pickle.dump(scores, f, pickle.HIGHEST_PROTOCOL)
+    # %% Result
+    plt.subplot(1, 2, 1)
+    plt.plot(np.arange(1, len(scores)+1), scores)
+    plt.ylabel('Reward')
+    plt.xlabel('Episode #')
+    with open('experiment/scores.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(scores, f, pickle.HIGHEST_PROTOCOL)
 
-plt.subplot(1, 2, 2)
-plt.plot(np.arange(1, len(avg_reward_list)+1), avg_reward_list)
-plt.ylabel('Average Reward')
-plt.xlabel('Episode #')
-plt.show()
-with open('experiment/avg_reward_list.pickle', 'wb') as f:
-    # Pickle the 'data' dictionary using the highest protocol available.
-    pickle.dump(avg_reward_list, f, pickle.HIGHEST_PROTOCOL)
-
+    plt.subplot(1, 2, 2)
+    plt.plot(np.arange(1, len(avg_reward_list)+1), avg_reward_list)
+    plt.ylabel('Average Reward')
+    plt.xlabel('Episode #')
+    plt.show()
+    with open('experiment/avg_reward_list.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(avg_reward_list, f, pickle.HIGHEST_PROTOCOL)
